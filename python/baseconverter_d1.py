3b9d820c03456686a84e44cc0e798e33b0f24429
Zeugma4242 <5657824+Zeugma4242@users.noreply.github.com>
2023-08-07 18:36:13 -0400

    Add file for alternative baseconverter implementation to do

---
+++ python/baseconverter_d1.py
@@ -0,0 +1,17 @@
+# D1 code to open possibilities and write 
+# another verion of the algorithm differently
+
+
+import string
+
+print(string.digits)
+print(string.ascii_uppercase)
+print(string.digits+string.ascii_uppercase)
+
+
+value_to_digit = string.digits+string.ascii_uppercase
+
+print("digit of", 15, value_to_digit[15])
+
+
+# TODO #12 D2 rewrite algorithm with that base
