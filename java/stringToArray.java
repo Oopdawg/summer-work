34e2efe32685ecefd567a7ea01d132df29257e3b
Oopdawg <94260533+Oopdawg@users.noreply.github.com>
2023-08-07 23:01:54 +0200

    removes useless comment

--- java/stringToArray.java
+++ java/stringToArray.java
@@ -2,7 +2,7 @@ public class stringToArray {
     public static void main(String[] args){
         String myStr = "testingABCDEF01234";
 
-        String[] mySplitStr = myStr.split("");
+        String[] mySplitStr = myStr.split(""); // splits string
 
         for (String s : mySplitStr){
             System.out.println(s + " " + s.getClass().getName());
