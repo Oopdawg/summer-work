23f19b9aa9a3ff83e50ebd410eff46f24f24f369
Oopdawg <94260533+Oopdawg@users.noreply.github.com>
2023-08-15 22:25:03 +0200

    updated GiveChange to fit prompt

--- java/edx/Practice_FreeResponse/GiveChange.java
+++ java/edx/Practice_FreeResponse/GiveChange.java
@@ -13,9 +13,9 @@ public class GiveChange {
         int changeInPennies = (int) (changeAmount * 100);
         
         int quarters = getQuarters(changeInPennies);
-        int dimes = getDimes(changeInPennies - quarters * 25);
-        int nickels = getNickels(changeInPennies - quarters * 25 - dimes * 10);
-        int remainingPennies = getPennies(changeInPennies - quarters * 25 - dimes * 10 - nickels * 5);
+        int dimes = getDimes(changeInPennies, quarters);
+        int nickels = getNickels(changeInPennies, quarters, dimes);
+        int remainingPennies = getPennies(changeInPennies, quarters, dimes, nickels);
         
         System.out.println("Change breakdown: " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, " + remainingPennies +" pennies.");
     }
@@ -24,16 +24,16 @@ public class GiveChange {
         return changeInPennies / 25;
     }
     
-    public static int getDimes(int pennies) {
-        return pennies / 10;
+    public static int getDimes(int changeInPennies, int quarters) {
+        return (changeInPennies - quarters * 25) / 10;
     }
     
-    public static int getNickels(int pennies) {
-        return pennies / 5;
+    public static int getNickels(int changeInPennies, int quarters, int dimes) {
+        return (changeInPennies - quarters * 25 - dimes * 10) / 5;
     }
     
-    public static int getPennies(int pennies) {
-        return pennies;
+    public static int getPennies(int changeInPennies, int quarters, int dimes, int nickels) {
+        return changeInPennies - quarters * 25 - dimes * 10 - nickels * 5;
     }
 
     public static void main(String[] args) {
